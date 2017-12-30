f7619711b8d0e814df1411a0b5dd90f75943eee1
Kalin <anastasovkalin@gmail.com>
2017-12-31 00:51:19 +0200

    Make temporary changes in to user's widget. Display users.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -346,25 +346,9 @@
 
 				  <!-- Default box -->
 				  <div class="box">
-				    <div class="box-header with-border">
-				      <h3 class="box-title">Title</h3>
-
-				      <div class="box-tools pull-right">
-				        <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-				                title="Collapse">
-				          <i class="fa fa-minus"></i></button>
-				        <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-				          <i class="fa fa-times"></i></button>
-				      </div>
-				    </div>
-				    <div class="box-body">
+				    
 				    	@yield('content')
-				    </div>
-				    <!-- /.box-body -->
-				    <div class="box-footer">
-				      Footer
-				    </div>
-				    <!-- /.box-footer-->
+				    
 				  </div>
 				  <!-- /.box -->
 
@@ -626,7 +610,6 @@
 				});
 			});
 			$(document).ready(function(){
-				
 				$("#example1_filter").addClass("pull-right");
 			})
 		</script>
